f8d361e903f95e4daab9bdbe1f384f2a439a9a3f
imranshuvo <imrankhanshuvo@gmail.com>
2022-05-17 00:29:21 +0600

    Initialize the canvas

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ canvas.width = 300;
 canvas.height = 300;
 
 image.onload = function(){
+    ctx.globalAlpha = 0.3;
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height );
+    ctx.globalAlpha = 1;
+
+    drawMe();
 }
 
+function drawMe(){
+    //ctx.beginPath(); //Begins the path
+    ctx.moveTo(150,20);  //Start the line to draw
+    ctx.lineTo(137,261); //upto this point from .moveTo
+    ctx.moveTo(45,158);
+    ctx.lineTo(238, 164)
+    ctx.stroke(); //Draw the line now
+}
+
+
+// Event Listeners
+canvas.addEventListener('click', (event) => {
+    console.log(event.x, event.y);
+});
